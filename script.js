51b4a9476f999d4332dc5f71efd7300d05b14873
Ekaterina Blagoeva <erblagoeva@gmail.com>
2020-01-09 18:38:26 -0500

    changed prompt to confirms

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ generateBtn.addEventListener("click", writePassword)
 
 //Prompts and confirms (FIGURE OUT THE PROMPT IF CANCEL WAS SELECTED!!!!!)
 var length = prompt("Please, enter the desired lenght of characters for your password, between 8 and 128.");
-var lowerCase = prompt("Would you like to include lower case characters?");
-var upperCase = prompt("Would you like to include upper case characters?");
-var numbers = prompt("Would you like to include numbers?");
+var lowerCase = confirm("Would you like to include lower case characters?");
+var upperCase = confirm("Would you like to include upper case characters?");
+var numbers = confirm("Would you like to include numbers?");
 var specialChar = confirm("Would you like to include special characters?");
