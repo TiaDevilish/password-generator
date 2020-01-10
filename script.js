0a32bb1d00135a895015e6764ae4c430b8afb9fa
Ekaterina Blagoeva <erblagoeva@gmail.com>
2020-01-10 15:39:03 -0500

    while if cancel on all chars added

--- script.js
+++ script.js
@@ -1,2 +1,43 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword)
+
+//Mine from this line down
+
+//prompt for lenght of char
+var length = prompt("Please, enter the desired lenght of characters for your password, between 8 and 128.");
+for (var i = length; i <= 8 || i >= 128; length = prompt("Please, enter the desired lenght of characters for your password, between 8 and 128.")) {
+  console.log(i);
+
+  if (length < 8 || length > 128) {
+    alert("The length of your password should be between 8 and 128 characters!");
+  }
+  else if (length >= 8 && length <= 128){
+    break;
+  }
+}
+
+//confirsm for password choises
+var lowerCase = confirm("Would you like to include lower case characters?");
+var upperCase = confirm("Would you like to include upper case characters?");
+var numbers = confirm("Would you like to include numbers?");
+var specialChar = confirm("Would you like to include special characters?");
+//if nothing was selected
+while (!lowerCase && !upperCase && !numbers && !specialChar) {
+  alert("You have to select at least one character type!")
+  lowerCase = confirm("Would you like to include lower case characters?");
+  upperCase = confirm("Would you like to include upper case characters?");
+  numbers = confirm("Would you like to include numbers?");
+  specialChar = confirm("Would you like to include special characters?");
+}
